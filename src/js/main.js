0c9b9c26dcc57f5a8c5eb1fbef8aeeea8a03b13f
Deepesh Nair <hybridx18@gmail.com>
2020-06-06 11:57:09 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,11 @@ import html from 'html-template-tag';
 
 document.querySelector('body').innerHTML = html`
 <main class="main"></main>
-<footer>
-    <copyright-x><span slot="name">hybridX</span></copyright-x>
-</footer>
 `;
 
 document.querySelector('main').innerHTML = html`
 <div class="main__text">hybridX</div>
 <div class="main__break-line"></div>
 <div class="main__text">& Code</div>
+ <copyright-x><span slot="name">hybridX</span></copyright-x>
 `;
