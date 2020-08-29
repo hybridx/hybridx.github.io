e9784178d03c279494f648547b769ff8e623381b
hybridx <hybridx18@gmail.com>
2020-08-29 18:57:14 +0530

    Added dev badge

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ document.querySelector('body').innerHTML = html`
 `;
 
 document.querySelector('main').innerHTML = html`
+<a href="https://dev.to/hybridx">
+  <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Deepesh Nair's DEV Profile" height="30" width="30">
+</a>
 <div class="main__text">hybridX</div>
 <div class="main__break-line"></div>
 <div class="main__text">& Code</div>
